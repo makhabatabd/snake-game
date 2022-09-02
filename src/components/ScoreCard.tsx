f739c9b1e7aa84c8a4906c66d9c474289073ad9d
Zhaina Almazbekova <jainaalmaz@gmail.com>
2022-09-02 17:18:25 +0600

    created

---
+++ src/components/ScoreCard.tsx
@@ -0,0 +1,14 @@
+import { Heading } from '@chakra-ui/react';
+import { useSelector } from 'react-redux';
+import { IGlobalState } from '../store/reducers/index';
+
+const ScoreCard = () => {
+  const score = useSelector((state: IGlobalState) => state.score);
+  return (
+    <Heading as="h2" size="md" mt={5} mb={5}>
+      Current Score: {score}
+    </Heading>
+  );
+};
+
+export default ScoreCard;
